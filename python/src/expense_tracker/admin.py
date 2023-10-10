1676992a4ff7fab9c5ba4387fb48bf7d1051281e
tom <tom.haeck@midiagnostics.com>
2023-10-10 20:57:10 +0200

    register models

--- python/src/expense_tracker/admin.py
+++ python/src/expense_tracker/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from expense_tracker.models import Organization, Account, Transfer
+
 
 # Register your models here.
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Transfer)
+class TransferAdmin(admin.ModelAdmin):
+    pass
