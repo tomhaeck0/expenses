578cf4972dd174534e76acae5a09070dbfb92dd1
tom <tom.haeck@midiagnostics.com>
2023-10-10 21:47:46 +0200

    customize the admin console

--- python/src/expense_tracker/admin.py
+++ python/src/expense_tracker/admin.py
@@ -5,14 +5,26 @@ from expense_tracker.models import Organization, Account, Transfer
 # Register your models here.
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("name",)
 
 
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("number", "name", "organization",)
 
 
 @admin.register(Transfer)
 class TransferAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("reference",
+                    "account_from",
+                    "account_to",
+                    "account_to_name",
+                    "amount",
+                    "currency",
+                    "date",
+                    "description",
+                    "comment",)
+
+    @staticmethod
+    def account_to_name(obj):
+        return obj.account_to.name
